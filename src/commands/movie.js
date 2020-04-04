8edadd5e253ca0edba49956dd0b5f15c4e2c1474
dj0nny <frank96990@gmail.com>
2020-04-04 16:53:28 +0200

    edit !movie suggest command output

--- src/commands/movie.js
+++ src/commands/movie.js
@@ -8,7 +8,7 @@ module.exports = async (msg, args) => {
       const res = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`);
       const suggestions = res.data.results;
       const movie = suggestions[Math.floor(Math.random() * 21)];
-      const reply = `${movie.title}\nhttps://image.tmdb.org/t/p/w220_and_h330_face${movie.poster_path}`;
+      const reply = `**Title**: ${movie.title}\n**Plot**: ${movie.overview}\nhttps://image.tmdb.org/t/p/w220_and_h330_face${movie.poster_path}`;
       msg.channel.send(reply);
       break;
 
