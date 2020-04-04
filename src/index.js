b30fe52f277e00c075d5585978d90e654f1ae53b
dj0nny <frank96990@gmail.com>
2020-04-04 11:42:04 +0200

    bot setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log("Alright ramblers, let's get rambling");
+});
+
+
+client.login(process.env.BOT_TOKEN);
